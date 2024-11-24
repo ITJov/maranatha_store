43205cfe96aa8b04669e5d28d25a7a443a326838
jssicalvina <2272004@maranatha.ac.id>
2024-11-24 20:29:39 +0700

    update index.blade.php
    memperbaiki tampilan dashboard admin

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,7 @@
 @endcomponent
 
 <div class="row">
-    <div class="col-md-6 col-xl-3">
+    <div class="col-md-6">
         <div class="card">
             <div class="card-body">
                 <div class="float-end mt-2">
@@ -17,13 +17,127 @@
                     <h4 class="mb-1 mt-1">$<span data-plugin="counterup">34,152</span></h4>
                     <p class="text-muted mb-0">Total Revenue</p>
                 </div>
-                <p class="text-muted mt-3 mb-0"><span class="text-success me-1"><i class="mdi mdi-arrow-up-bold me-1"></i>2.65%</span> since last week
-                </p>
+                <!-- <p class="text-muted mt-3 mb-0"><span class="text-success me-1"><i class="mdi mdi-arrow-up-bold me-1"></i>2.65%</span>  last week
+                </p> -->
             </div>
         </div>
-    </div> <!-- end col-->
+    </div> 
 
-    <div class="col-md-6 col-xl-3">
+    <div class="col-md-6">
+        <div class="card">
+            <div class="card-body">
+                <div class="float-end mt-2">
+                    <div id="total-revenue-chart" data-colors='["--bs-primary"]'></div>
+                </div>
+                <div>
+                    <h4 class="mb-1 mt-1">$<span data-plugin="counterup">34,152</span></h4>
+                    <p class="text-muted mb-0">Total Revenue</p>
+                </div>
+                <!-- <p class="text-muted mt-3 mb-0"><span class="text-success me-1"><i class="mdi mdi-arrow-up-bold me-1"></i>2.65%</span>  last week
+                </p> -->
+            </div>
+        </div>
+    </div> 
+</div>
+
+<div class="card">
+        <div class="card-body">
+            <div class="float-end">
+                <div class="dropdown">
+                    <a class="dropdown-toggle text-reset" href="#" id="dropdownMenuButton1"
+                        data-bs-toggle="dropdown" aria-haspopup="true"
+                        aria-expanded="false">
+                        <span class="fw-semibold">Sort By:</span> <span class="text-muted">Yearly<i class="mdi mdi-chevron-down ms-1"></i></span>
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton1">
+                        <a class="dropdown-item" href="#">Monthly</a>
+                        <a class="dropdown-item" href="#">Yearly</a>
+                        <a class="dropdown-item" href="#">Weekly</a>
+                    </div>
+                </div>
+            </div>
+
+            <h4 class="card-title mb-4">Top Selling Products</h4>
+
+
+            <div class="row align-items-center g-0 mt-3">
+                <div class="col-sm-3">
+                    <p class="text-truncate mt-1 mb-0"><i class="mdi mdi-circle-medium text-primary me-2"></i> Roti </p>
+                </div>
+
+                <div class="col-sm-9">
+                    <div class="progress mt-1" style="height: 6px;">
+                        <div class="progress-bar progress-bar bg-primary" role="progressbar"
+                            style="width: 52%" aria-valuenow="52" aria-valuemin="0"
+                            aria-valuemax="52">
+                        </div>
+                    </div>
+                </div>
+            </div> <!-- end row-->
+
+            <div class="row align-items-center g-0 mt-3">
+                <div class="col-sm-3">
+                    <p class="text-truncate mt-1 mb-0"><i class="mdi mdi-circle-medium text-info me-2"></i> Es Krim </p>
+                </div>
+                <div class="col-sm-9">
+                    <div class="progress mt-1" style="height: 6px;">
+                        <div class="progress-bar progress-bar bg-info" role="progressbar"
+                            style="width: 45%" aria-valuenow="45" aria-valuemin="0"
+                            aria-valuemax="45">
+                        </div>
+                    </div>
+                </div>
+            </div> <!-- end row-->
+
+            <div class="row align-items-center g-0 mt-3">
+                <div class="col-sm-3">
+                    <p class="text-truncate mt-1 mb-0"><i class="mdi mdi-circle-medium text-success me-2"></i> Indomie </p>
+                </div>
+                <div class="col-sm-9">
+                    <div class="progress mt-1" style="height: 6px;">
+                        <div class="progress-bar progress-bar bg-success" role="progressbar"
+                            style="width: 48%" aria-valuenow="48" aria-valuemin="0"
+                            aria-valuemax="48">
+                        </div>
+                    </div>
+                </div>
+            </div> <!-- end row-->
+
+            <div class="row align-items-center g-0 mt-3">
+                <div class="col-sm-3">
+                    <p class="text-truncate mt-1 mb-0"><i class="mdi mdi-circle-medium text-warning me-2"></i> Susu </p>
+                </div>
+                <div class="col-sm-9">
+                    <div class="progress mt-1" style="height: 6px;">
+                        <div class="progress-bar progress-bar bg-warning" role="progressbar"
+                            style="width: 78%" aria-valuenow="78" aria-valuemin="0"
+                            aria-valuemax="78">
+                        </div>
+                    </div>
+                </div>
+            </div> <!-- end row-->
+
+            <div class="row align-items-center g-0 mt-3">
+                <div class="col-sm-3">
+                    <p class="text-truncate mt-1 mb-0"><i class="mdi mdi-circle-medium text-purple me-2"></i> Brownies </p>
+                </div>
+                <div class="col-sm-9">
+                    <div class="progress mt-1" style="height: 6px;">
+                        <div class="progress-bar progress-bar bg-purple" role="progressbar"
+                            style="width: 63%" aria-valuenow="63" aria-valuemin="0"
+                            aria-valuemax="63">
+                        </div>
+                    </div>
+                </div>
+            </div> <!-- end row-->
+
+        </div> <!-- end card-body-->
+    </div> <!-- end card-->
+</div> <!-- end Col -->
+
+ 
+    <!-- <div class="col-md-6 col-xl-3">
         <div class="card">
             <div class="card-body">
                 <div class="float-end mt-2">
@@ -37,9 +151,9 @@
                 </p>
             </div>
         </div>
-    </div> <!-- end col-->
+    </div> end col -->
 
-    <div class="col-md-6 col-xl-3">
+    <!-- <div class="col-md-6 col-xl-3">
         <div class="card">
             <div class="card-body">
                 <div class="float-end mt-2">
@@ -53,8 +167,8 @@
                 </p>
             </div>
         </div>
-    </div> <!-- end col-->
-
+    </div> end col -->
+<!-- 
     <div class="col-md-6 col-xl-3">
 
         <div class="card">
@@ -70,149 +184,49 @@
                 </p>
             </div>
         </div>
-    </div> <!-- end col-->
-</div> <!-- end row-->
+    </div> end col -->
+<!-- </div> end row -->
 
-<div class="row">
-    <div class="col-xl-8">
-        <div class="card">
-            <div class="card-body">
-                <div class="float-end">
-                    <div class="dropdown">
-                        <a class="dropdown-toggle text-reset" href="#" id="dropdownMenuButton5" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <span class="fw-semibold">Sort By:</span> <span class="text-muted">Yearly<i class="mdi mdi-chevron-down ms-1"></i></span>
-                        </a>
+<div class="card">
+    <div class="card-body">
+        <div class="float-end">
+            <div class="dropdown">
+                <a class="dropdown-toggle text-reset" href="#" id="dropdownMenuButton5" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <span class="fw-semibold">Sort By:</span> <span class="text-muted">Yearly<i class="mdi mdi-chevron-down ms-1"></i></span>
+                </a>
 
-                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton5">
-                            <a class="dropdown-item" href="#">Monthly</a>
-                            <a class="dropdown-item" href="#">Yearly</a>
-                            <a class="dropdown-item" href="#">Weekly</a>
-                        </div>
-                    </div>
+                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton5">
+                    <a class="dropdown-item" href="#">Monthly</a>
+                    <a class="dropdown-item" href="#">Yearly</a>
+                    <a class="dropdown-item" href="#">Weekly</a>
                 </div>
-                <h4 class="card-title mb-4">Sales Analytics</h4>
+            </div>
+        </div>
+        <h4 class="card-title mb-4">Sales Analytics</h4>
 
-                <div class="mt-1">
-                    <ul class="list-inline main-chart mb-0">
-                        <li class="list-inline-item chart-border-left me-0 border-0">
-                            <h3 class="text-primary">$<span data-plugin="counterup">2,371</span><span class="text-muted d-inline-block font-size-15 ms-3">Income</span></h3>
-                        </li>
-                        <li class="list-inline-item chart-border-left me-0">
-                            <h3><span data-plugin="counterup">258</span><span class="text-muted d-inline-block font-size-15 ms-3">Sales</span>
-                            </h3>
-                        </li>
-                        <li class="list-inline-item chart-border-left me-0">
-                            <h3><span data-plugin="counterup">3.6</span>%<span class="text-muted d-inline-block font-size-15 ms-3">Conversation Ratio</span></h3>
-                        </li>
-                    </ul>
-                </div>
+        <div class="mt-1">
+            <ul class="list-inline main-chart mb-0">
+                <li class="list-inline-item chart-border-left me-0 border-0">
+                    <h3 class="text-primary">$<span data-plugin="counterup">2,371</span><span class="text-muted d-inline-block font-size-15 ms-3">Income</span></h3>
+                </li>
+                <li class="list-inline-item chart-border-left me-0">
+                    <h3><span data-plugin="counterup">258</span><span class="text-muted d-inline-block font-size-15 ms-3">Sales</span>
+                    </h3>
+                </li>
+                <li class="list-inline-item chart-border-left me-0">
+                    <h3><span data-plugin="counterup">3.6</span>%<span class="text-muted d-inline-block font-size-15 ms-3">Conversation Ratio</span></h3>
+                </li>
+            </ul>
+        </div>
 
-                <div class="mt-3">
-                    <div id="sales-analytics-chart" data-colors='["--bs-primary", "#dfe2e6", "--bs-warning"]' class="apex-charts" dir="ltr"></div>
-                </div>
-            </div> <!-- end card-body-->
-        </div> <!-- end card-->
-    </div> <!-- end col-->
-
-
-        <div class="card">
-            <div class="card-body">
-                <div class="float-end">
-                    <div class="dropdown">
-                        <a class="dropdown-toggle text-reset" href="#" id="dropdownMenuButton1"
-                            data-bs-toggle="dropdown" aria-haspopup="true"
-                            aria-expanded="false">
-                            <span class="fw-semibold">Sort By:</span> <span class="text-muted">Yearly<i class="mdi mdi-chevron-down ms-1"></i></span>
-                        </a>
-
-                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton1">
-                            <a class="dropdown-item" href="#">Monthly</a>
-                            <a class="dropdown-item" href="#">Yearly</a>
-                            <a class="dropdown-item" href="#">Weekly</a>
-                        </div>
-                    </div>
-                </div>
-
-                <h4 class="card-title mb-4">Top Selling Products</h4>
+        <div class="mt-3">
+            <div id="sales-analytics-chart" data-colors='["--bs-primary", "#dfe2e6", "--bs-warning"]' class="apex-charts" dir="ltr"></div>
+        </div>
+    </div> <!-- end card-body-->
+</div> <!-- end card-->
 
 
-                <div class="row align-items-center g-0 mt-3">
-                    <div class="col-sm-3">
-                        <p class="text-truncate mt-1 mb-0"><i class="mdi mdi-circle-medium text-primary me-2"></i> Roti </p>
-                    </div>
-
-                    <div class="col-sm-9">
-                        <div class="progress mt-1" style="height: 6px;">
-                            <div class="progress-bar progress-bar bg-primary" role="progressbar"
-                                style="width: 52%" aria-valuenow="52" aria-valuemin="0"
-                                aria-valuemax="52">
-                            </div>
-                        </div>
-                    </div>
-                </div> <!-- end row-->
-
-                <div class="row align-items-center g-0 mt-3">
-                    <div class="col-sm-3">
-                        <p class="text-truncate mt-1 mb-0"><i class="mdi mdi-circle-medium text-info me-2"></i> Es Krim </p>
-                    </div>
-                    <div class="col-sm-9">
-                        <div class="progress mt-1" style="height: 6px;">
-                            <div class="progress-bar progress-bar bg-info" role="progressbar"
-                                style="width: 45%" aria-valuenow="45" aria-valuemin="0"
-                                aria-valuemax="45">
-                            </div>
-                        </div>
-                    </div>
-                </div> <!-- end row-->
-
-                <div class="row align-items-center g-0 mt-3">
-                    <div class="col-sm-3">
-                        <p class="text-truncate mt-1 mb-0"><i class="mdi mdi-circle-medium text-success me-2"></i> Indomie </p>
-                    </div>
-                    <div class="col-sm-9">
-                        <div class="progress mt-1" style="height: 6px;">
-                            <div class="progress-bar progress-bar bg-success" role="progressbar"
-                                style="width: 48%" aria-valuenow="48" aria-valuemin="0"
-                                aria-valuemax="48">
-                            </div>
-                        </div>
-                    </div>
-                </div> <!-- end row-->
-
-                <div class="row align-items-center g-0 mt-3">
-                    <div class="col-sm-3">
-                        <p class="text-truncate mt-1 mb-0"><i class="mdi mdi-circle-medium text-warning me-2"></i> Susu </p>
-                    </div>
-                    <div class="col-sm-9">
-                        <div class="progress mt-1" style="height: 6px;">
-                            <div class="progress-bar progress-bar bg-warning" role="progressbar"
-                                style="width: 78%" aria-valuenow="78" aria-valuemin="0"
-                                aria-valuemax="78">
-                            </div>
-                        </div>
-                    </div>
-                </div> <!-- end row-->
-
-                <div class="row align-items-center g-0 mt-3">
-                    <div class="col-sm-3">
-                        <p class="text-truncate mt-1 mb-0"><i class="mdi mdi-circle-medium text-purple me-2"></i> Brownies </p>
-                    </div>
-                    <div class="col-sm-9">
-                        <div class="progress mt-1" style="height: 6px;">
-                            <div class="progress-bar progress-bar bg-purple" role="progressbar"
-                                style="width: 63%" aria-valuenow="63" aria-valuemin="0"
-                                aria-valuemax="63">
-                            </div>
-                        </div>
-                    </div>
-                </div> <!-- end row-->
-
-            </div> <!-- end card-body-->
-        </div> <!-- end card-->
-    </div> <!-- end Col -->
-</div> <!-- end row-->
-
-<div class="row">
+<!-- <div class="row">
     <div class="col-lg-12">
         <div class="card">
             <div class="card-body">
@@ -404,11 +418,11 @@
                         </tbody>
                     </table>
                 </div>
-                <!-- end table-responsive -->
-            </div>
+                 end table-responsive -->
+            <!-- </div>
         </div>
     </div>
-</div>
+</div>  -->
 <!-- end row -->
 
 @endsection
